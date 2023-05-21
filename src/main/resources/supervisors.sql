f2f8b5cf96b54c61f6bfec556f3c181876f8d48c
GiorgiaAuroraAdorni <adornigiorgia@outlook.it>
2023-05-21 14:02:51 +0200

    Updated supervisors list

--- src/main/resources/supervisors.sql
+++ src/main/resources/supervisors.sql
@@ -10,12 +10,17 @@ create unique index supervisors_id_uindex
     on supervisors (id);
 
 INSERT INTO supervisors (id, full_name) VALUES (1, 'Giorgia Adorni');
-INSERT INTO supervisors (id, full_name) VALUES (2, 'Alberto Piatti');
-INSERT INTO supervisors (id, full_name) VALUES (3, 'Luca Gambardella');
-INSERT INTO supervisors (id, full_name) VALUES (4, 'Francesca Mangili');
-INSERT INTO supervisors (id, full_name) VALUES (5, 'Francesco Mondada');
-INSERT INTO supervisors (id, full_name) VALUES (6, 'Lucio Negrini');
-INSERT INTO supervisors (id, full_name) VALUES (7, 'Kunal Massé');
-INSERT INTO supervisors (id, full_name) VALUES (8, 'Engin Bumbacher');
-INSERT INTO supervisors (id, full_name) VALUES (9, 'Jérôme Brender');
-INSERT INTO supervisors (id, full_name) VALUES (10, 'Dorit Assaf');
+INSERT INTO supervisors (id, full_name) VALUES (2, 'Elia Lutz');
+INSERT INTO supervisors (id, full_name) VALUES (3, 'Kunal Massé');
+INSERT INTO supervisors (id, full_name) VALUES (4, 'Melissa Skweres');
+INSERT INTO supervisors (id, full_name) VALUES (5, 'Alberto Piatti');
+INSERT INTO supervisors (id, full_name) VALUES (6, 'Dorit Assaf');
+INSERT INTO supervisors (id, full_name) VALUES (7, 'Lucio Negrini');
+INSERT INTO supervisors (id, full_name) VALUES (8, 'Francesco Mondada');
+INSERT INTO supervisors (id, full_name) VALUES (9, 'Engin Bumbacher');
+INSERT INTO supervisors (id, full_name) VALUES (10, 'Jérôme Brender');
+INSERT INTO supervisors (id, full_name) VALUES (11, 'Luca Gambardella');
+INSERT INTO supervisors (id, full_name) VALUES (12, 'Francesca Mangili');
+
+
+
